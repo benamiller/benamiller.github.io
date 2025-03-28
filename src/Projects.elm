b1cce517be569756c91dfabe45d426dbfe802b34
benamiller <mrbenamiller@gmail.com>
2025-03-28 18:28:57 +0000

    Add link field for link to project

--- src/Projects.elm
+++ src/Projects.elm
@@ -8,6 +8,7 @@ type alias Project =
     { title : String
     , tech : List String
     , description : String
+    , link : String
     }
 
 
@@ -31,25 +32,31 @@ projects =
     [ { title = "Essence"
       , tech = [ "English", "Test" ]
       , description = "Essence description"
+      , link = "https://benmiller.ai"
       }
     , { title = "mlzero"
       , tech = [ "Next.js", "React", "PostgreSQL", "Prisma" ]
       , description = "mlzero description"
+      , link = "https://benmiller.ai"
       }
     , { title = "mlc"
       , tech = [ "English", "Test" ]
       , description = "mlc description"
+      , link = "https://benmiller.ai"
       }
     , { title = "Assassins"
       , tech = [ "English", "Test" ]
       , description = "Assassins description"
+      , link = "https://benmiller.ai"
       }
     , { title = "Immersive Gaming"
       , tech = [ "English", "Test" ]
       , description = "Immersive Gaming description"
+      , link = "https://benmiller.ai"
       }
     , { title = "Portfolio"
       , tech = [ "English", "Test" ]
       , description = "Elm portfolio description"
+      , link = "https://benmiller.ai"
       }
     ]
